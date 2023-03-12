b92adc0eb9d20a3ed7b55653308c11fbcba03549
Andrei Ispas <andreiispas838@yahoo.com>
2023-03-12 19:38:57 +0200

    finished project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+
+import MovieCard from './MovieCard';
+
+import './App.css';
+import searchIcon from './search.svg';
+
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_KEY}`;
 
 const App = () => {
-  return <div className='App'></div>;
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('Matrix');
+  }, []);
+
+  return (
+    <div className='app'>
+      <h1>MovieWiki</h1>
+      <div className='search'>
+        <input placeholder='Search for movies ' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={searchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+const container = createRoot(root);
+container.render(<App />);
